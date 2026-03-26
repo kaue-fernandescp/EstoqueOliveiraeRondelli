503648bf9bb43e319edb70a3c677710a2575a977
Kauê Fernandes <153867670+kaue-fernandescp@users.noreply.github.com>
2026-03-26 18:09:17 -0300

    Add files via upload
    Tela de registro de usuário.

--- EstoqueOliveiraeRondelli/produtos/views.py
+++ EstoqueOliveiraeRondelli/produtos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 from .models import Produtos, Unidades
 from .forms import ProdutosForm, UnidadesForm
 
--- EstoqueOliveiraeRondelli/usuarios/urls.py
+++ EstoqueOliveiraeRondelli/usuarios/urls.py
@@ -5,4 +5,5 @@ from usuarios import views
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
     path('logout', views.logout_view, name='logout'),
+    path('registrar', views.registrar_usuarios, name='registrar'),
 ]
