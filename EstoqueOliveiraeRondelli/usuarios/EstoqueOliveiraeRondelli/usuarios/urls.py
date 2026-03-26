3b6c26f3ba5b223df992794120a02435016b1906
Kauê Fernandes <153867670+kaue-fernandescp@users.noreply.github.com>
2026-03-26 18:08:17 -0300

    Delete EstoqueOliveiraeRondelli/usuarios/EstoqueOliveiraeRondelli directory

--- EstoqueOliveiraeRondelli/usuarios/EstoqueOliveiraeRondelli/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from usuarios import views
-
-urlpatterns = [
-    path('login', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('registrar', views.registrar_usuarios, name='registrar'),
-]
