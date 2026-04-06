1339d78ada46040d379a6f8b0d14dbfea227fd2a
Kauê Fernandes Costa Pereira <153867670+kaue-fernandescp@users.noreply.github.com>
2026-04-06 18:03:24 -0300

    Add files via upload
    Estilização parte 1;
    Novo filtro "Tipo" para as movimentações;
    Tela para editar usuários, alterando os campos Ativo e Administrador.

--- movimentacoes/filters.py
+++ movimentacoes/filters.py
@@ -9,6 +9,9 @@ class FiltroMovimentacao(django_filters.FilterSet):
     # Filtro por produto
     produto = django_filters.ModelChoiceFilter(field_name='mov_produto', queryset=Produtos.objects.all(), label='Produto:')
 
+    # Filtor por tipo de movimentação
+    tipo = django_filters.ChoiceFilter(field_name='mov_tipo', choices=Movimentacao.TIPO_MOVIMENTACAO, label='Tipo:')
+
     # Filtro por intervalo de datas
     data_inicial = django_filters.DateFilter(field_name='mov_data_adicionada', lookup_expr='gte', label='De:', widget=forms.DateInput(attrs={'type': 'date'}))
     data_final = django_filters.DateFilter(field_name='mov_data_adicionada', lookup_expr='lte', label='Até:', widget=forms.DateInput(attrs={'type': 'date'}))
@@ -16,4 +19,4 @@ class FiltroMovimentacao(django_filters.FilterSet):
     # Metadados para o filtro
     class Meta:
         model = Movimentacao
-        fields = ['produto', 'data_inicial', 'data_final']
+        fields = ['produto', 'tipo', 'data_inicial', 'data_final']
